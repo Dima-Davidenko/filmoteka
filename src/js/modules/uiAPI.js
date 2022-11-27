4060d6f6abdf0f60c7e84edb659df4ce7e142c0b
Volin13 <110535249+Volin13@users.noreply.github.com>
2022-11-27 21:05:44 +0200

    тест 1

--- src/js/modules/uiAPI.js
+++ src/js/modules/uiAPI.js
@@ -1,4 +1,8 @@
 import galleryElement from '../../templates/galleryElement.hbs';
+import modal from '../../templates/modal.hbs';
+
+const genersEl = document.querySelectorAll('.item__category-js');
+
 const galleryContainer = document.querySelector('.js-gallery');
 
 export const uiAPI = {
@@ -18,6 +22,18 @@ export const uiAPI = {
 
     // console.log(galleryElement);
     galleryContainer.innerHTML = galleryElement(moviesListInfo);
+    // galleryContainer.innerHTML = modal(moviesListInfo);
+
+      // genersEl.forEach(element => {
+      //   // console.log(element.textContent.length);
+      //   // element.textContent.split(" ").join(",")
+      //   if (element.textContent.endsWith(',')) {
+      //     // element.textContent.splice(element.textContent.length - 1, 1)
+      //     // const text = element.textContent;
+      //     // text.slice(0, text.length - 1);
+      //     // console.log(text.slice(0, 3));
+      //   }
+      // });
   },
   renderHeader: () => {},
 };
